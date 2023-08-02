2ff8096837b7821a404ec3c8bdd5585aa04ce9af
IsraelSDev <israelsoares007@outlook.com>
2023-08-02 14:33:33 -0300

    página medicos dinâmica

--- .next/static/webpack/webpack.911525cdcde03a9a.hot-update.js
+++ .next/static/webpack/webpack.911525cdcde03a9a.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "794393540b4e22f1"; }
+/******/ 	__webpack_require__.h = function() { return "a1c7b35d3830ad13"; }
 /******/ }();
 /******/ 
 /******/ }
--- components/Medicos/MedicoConsulta.jsx
+++ components/Medicos/MedicoConsulta.jsx
@@ -1,3 +1,27 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5191733ecf725232f065dae1627d7037e59ad8c8b90fd375aaf341d5c4342c7d
-size 853
+
+import MedicoSingle from "./MedicoSingle";
+import {Button} from "../Button/Button";
+import line from '/public/images/line.svg'
+import Image from "next/image";
+import styles from '/styles/medicos.module.sass'
+
+const medicoConsulta=({medico}) => (<>
+    <div className={styles.docConsulta}>
+        <div className={'d-flex align-items-center'}>
+            <MedicoSingle medico={medico} />
+            <div className={styles.boxCall}>
+                <div className={'d-flex flex-column justify-content-between h-100'}>
+                    <Button id={1} title={'Marcar Consulta'} />
+                    <Image src={line} width={172} height={2} />
+                    <p>
+                        Unidade barra da tijuca
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</>)
+
+
+export default medicoConsulta;
